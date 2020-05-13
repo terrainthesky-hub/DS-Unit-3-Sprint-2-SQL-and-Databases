a4e800ac0527a5f89b5bddac6a074053712253a8
Lesley Rich <lesley.t.rich@gmail.com>
2020-05-13 16:13:00 -0700

    mongo db

--- module3-nosql-and-document-oriented-databases/rpg_mongo.py
+++ module3-nosql-and-document-oriented-databases/rpg_mongo.py
@@ -17,14 +17,14 @@ CLUSTER_NAME = os.getenv("MONGO_CLUSTER_NAME", default="OOPS")
 connection_uri = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{CLUSTER_NAME}.mongodb.net/test?retryWrites=true&w=majority"
 #Change these values as needed
 query = """
-SELECT * FROM armory_item
+SELECT * FROM armory_item;
 """
 
 client = pymongo.MongoClient(connection_uri)
 
 consq=sqlite3.connect('rpg_db.sqlite3')
 
-engine = create_engine(r'sqlite:///C:\Users\lesle\Desktop\Lambda_sql_sprint\DS-Unit-3-Sprint-2-SQL-and-Databases\module3-nosql-and-document-oriented-databases\rpg_db.sqlite3')
+engine = create_engine(r'sqlite:///rpg_db.sqlite3')
 
 
 df = pd.read_sql(query, con=engine)
@@ -39,9 +39,11 @@ armory = db.rpg_armory
 # df = pd.Series(df).apply(eval)
 # df2 = pd.DataFrame(df.tolist(), columns=['item_id', 'name', 'value', 'weight'])
 records = json.loads(df.T.to_json()).values()
-armory.insert(records)
+#records_dict = df.to_dict("records")
+#records = json.loads(df.to_json())
+armory.insert_many(records)
 
-breakpoint()
-armory.insert_many(df)
+
+#armory.insert_many(df)
 
 equip = armory.find_one({"name": "Qui"})
